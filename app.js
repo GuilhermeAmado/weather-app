811587703e32cfdbcebb410f9fab3372eccf6f5c
Guilherme <guiii@live.com>
2020-10-10 16:39:24 -0300

    Add geocoding request

--- app.js
+++ app.js
@@ -6,3 +6,12 @@ request({ url, json: true }, (error, response) => {
     const currentWeather = response.body.current;
     console.log(`${currentWeather.weather_descriptions[0]}. It is currently ${currentWeather.temperature} degrees out. It feels like ${currentWeather.feelslike} degrees out.`);
 });
+
+// Geocoding
+const geocodeURL = `https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiYW1hZG9ndWkiLCJhIjoiY2tnMmxtN3J4MDE4czJyanQ2N20wemswMCJ9.zlJ_1rSVl2qQIZPMlMPYJQ&limit=1`;
+
+request({ url: geocodeURL, json: true }, (error, response) => {
+    const latitude = response.body.features[0].center[1];
+    const longitude = response.body.features[0].center[0];
+    console.log(latitude, longitude);
+});
